fa662abec732aa22e423419123b9975704cb10ed
picongzhi <picongzhi@gmail.com>
2019-10-21 01:44:36 +0800

    feat(spittr): add web mvc config

---
+++ spittr/src/main/java/com/pcz/spittr/data/JdbcSpitterRepository.java
@@ -0,0 +1,57 @@
+package com.pcz.spittr.data;
+
+import com.pcz.spittr.Spitter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author picongzhi
+ */
+@Repository
+public class JdbcSpitterRepository implements SpitterRepository {
+    private JdbcOperations jdbc;
+
+    @Autowired
+    public JdbcSpitterRepository(JdbcOperations jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @Override
+    public Spitter save(Spitter spitter) {
+        jdbc.update("INSERT INTO Spitter " +
+                        "(username, password, first_name, last_name, email)" +
+                        " VALUES (?, ?, ?, ?, ?)",
+                spitter.getUsername(),
+                spitter.getPassword(),
+                spitter.getFirstName(),
+                spitter.getLastName(),
+                spitter.getEmail());
+
+        return spitter;
+    }
+
+    @Override
+    public Spitter findByUsername(String username) {
+        return jdbc.queryForObject("SELECT id, username, null, first_name, last_name, email" +
+                        " FROM Spittr WHERE username = ?",
+                new SpitterRowMapper(), username);
+    }
+
+    private static class SpitterRowMapper implements RowMapper<Spitter> {
+        @Override
+        public Spitter mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Spitter(
+                    rs.getLong("id"),
+                    rs.getString("username"),
+                    null,
+                    rs.getString("first_name"),
+                    rs.getString("last_name"),
+                    rs.getString("email"));
+        }
+    }
+}
---
+++ spittr/src/main/java/com/pcz/spittr/web/WebConfig.java
@@ -0,0 +1,38 @@
+package com.pcz.spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author picongzhi
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.pcz.spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+    }
+}
