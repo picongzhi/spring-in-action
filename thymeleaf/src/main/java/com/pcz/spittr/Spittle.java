91a94c90a8d0c23f3ee8a31e04ab2747ae7118bd
picongzhi <picongzhi@gmail.com>
2019-12-20 19:12:35 +0800

    feat(chapter06): add thymeleaf

---
+++ thymeleaf/src/main/java/com/pcz/spittr/Spittle.java
@@ -0,0 +1,69 @@
+package com.pcz.spittr;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.Date;
+
+/**
+ * @author picongzhi
+ */
+public class Spittle {
+    private final Long id;
+    private final String message;
+    private final Date time;
+    private Double longitude;
+    private Double latitude;
+
+    public Spittle(String message, Date time) {
+        this(null, message, time, null, null);
+    }
+
+    public Spittle(Long id, String message, Date time, Double longitude, Double latitude) {
+        this.id = id;
+        this.message = message;
+        this.time = time;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj,
+                "id", "time");
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this,
+                "id", "time");
+    }
+}
